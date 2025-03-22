c1411f802d8ffa49eb6346683e5239a15e768011
Eduard-Gergil <nifigasegamer@gmail.com>
2025-03-22 19:23:37 +0500

    feat: add header with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, Link } from 'react-router'
 import './App.css'
 import Home from './pages/Home'
 import Cart from './pages/Cart'
@@ -6,6 +6,15 @@ import Cart from './pages/Cart'
 function App() {
   return (
     <BrowserRouter>
+      <nav className="flex space-x-4 mb-4">
+        <Link to="/" className="text-blue-500 hover:underline">
+          Главная
+        </Link>
+        <Link to="/cart" className="text-blue-500 hover:underline">
+          Корзина
+        </Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
